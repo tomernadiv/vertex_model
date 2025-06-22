59e7ba1ccf0aec2e8753d93ff0cc9a4acd9853bd
tomerna <tomerna@login4.wexac.weizmann.ac.il>
2025-06-22 14:53:55 +0300

    fix coef

--- configs/morphology_config.py
+++ configs/morphology_config.py
@@ -1,5 +1,5 @@
 num_rows = 60
 num_cols = 60
-num_layers = 10 # of each outline of the frame
-num_frames = 2
+num_layers = 10    # of each outline of the frame
+num_frames = 2     # 2
 neurons_out = True
